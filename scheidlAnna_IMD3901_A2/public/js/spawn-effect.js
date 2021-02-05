440e3e387c57d2913f872eff913cb229349ec67c
annascheidl <60186133+annascheidl@users.noreply.github.com>
2021-02-04 23:17:02 -0500

    Add files via upload

--- scheidlAnna_IMD3901_A2/public/js/spawn-effect.js
+++ scheidlAnna_IMD3901_A2/public/js/spawn-effect.js
@@ -12,7 +12,11 @@ AFRAME.registerComponent('spawn-effect', {
             console.log('Spawn duck');
             var ascene = document.querySelector('a-scene');
             var ducky = document.createElement('ducky');
+            var sphere = document.createElement('a-sphere');
             ascene.appendChild(ducky);
+            ascene.appendChild(sphere);
+            CONTEXT_AF.sphere.setAttribute('a-sphere', {property:'position', to: '0 0.5 -10'})
+            
             CONTEXT_AF.ducky.setAttribute('ducky', {property:'position', to: '0 0.5 -10'})
             CONTEXT_AF.ducky.setAttribute('ducky', {property:'scale', to: '1 1 1'})
         })
